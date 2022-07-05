fe9239316040131b2c0dfbeb967e5d4e66387d85
BuildTools <gtpalm42@gmail.com>
2022-07-05 17:41:28 -0400

    Complixety Factor * 10

--- src/main/java/me/rebelmythik/anticopperdecay/events/CancelCopperEvent.java
+++ src/main/java/me/rebelmythik/anticopperdecay/events/CancelCopperEvent.java
@@ -1,23 +1,51 @@
 package me.rebelmythik.anticopperdecay.events;
 
-import me.rebelmythik.anticopperdecay.AntiCopperDecay;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFormEvent;
 
+import me.rebelmythik.anticopperdecay.AntiCopperDecay;
+
 public class CancelCopperEvent implements Listener {
 
-    public AntiCopperDecay plugin;
+    public final AntiCopperDecay plugin;
+    public final List<Material> copperMats;
 
     public CancelCopperEvent(AntiCopperDecay plugin) {
         this.plugin = plugin;
+        this.copperMats = Stream.of(Material.values()).filter(m -> m.toString().contains("COPPER")).collect(Collectors.toList());
     }
 
     @EventHandler
     public void CopperBlock(BlockFormEvent event) {
-        if (event.getBlock().getType().toString().contains("COPPER")) {
-            event.setCancelled(true);
+    	Chunk chunk = event.getBlock().getChunk();
+    	ArrayList<Block> copper = new ArrayList<Block>();
+        for(int y = -64; y <= 319; y++) {
+            for(int x = chunk.getX() * 16; x <= (x + 15); x++) {
+                for(int z = chunk.getX() * 16; z <= (x + 15); z++) {
+                	Location loc = new Location(chunk.getWorld(), x, y, z);
+                	Block currentBlock = chunk.getWorld().getBlockAt(loc);
+                    String[] name = currentBlock.getType().toString().split("_");
+                    int last = name.length - 1;
+                    if (name[last].contentEquals("COPPER")) {
+                    	copper.add(currentBlock);
+                    }
+                }
+            }
         }
+        copper.stream().forEachOrdered((block) -> {
+        	Ageable ageable = (Ageable) block.getBlockData();
+        	ageable.setAge(0);
+        });
     }
 }
